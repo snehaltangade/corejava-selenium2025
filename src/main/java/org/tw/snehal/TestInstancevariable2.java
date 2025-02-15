63d103f57cea7a076ae9dd3f75bdb0d48c6b0766
tanga <tangadesnehal6@gmail.com>
2025-02-15 18:07:27 +0530

    Added New Test Instance Variable

---
+++ src/main/java/org/tw/snehal/TestInstancevariable2.java
@@ -0,0 +1,11 @@
+package org.tw.snehal;
+
+public class TestInstancevariable2 {
+    public static void main(String[] args) {
+
+        TestStaticVariable1 obj = new TestStaticVariable1();
+        System.out.println(obj.a);
+
+        System.out.println(TestStaticVariable1.a);
+    }
+}
