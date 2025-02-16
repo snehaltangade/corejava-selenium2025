0637b29d5128899cae8dbf3e5d2fead1cd7a841a
tanga <tangadesnehal6@gmail.com>
2025-02-16 11:15:57 +0530

    Added a comments

---
+++ src/main/java/org/tw/snehal/TestComments.java
@@ -0,0 +1,21 @@
+package org.tw.snehal;
+/**
+ * This is my documentation comment
+ * @author Shriniwas
+ * @since 2025
+ */
+public class TestComments {
+    public static void main(String[] args) {
+
+        // Single Line comment:  Shortcut: Ctrl+/
+       //    System.out.println("Line1");
+
+        // Multi Line Comments: Shortcut: Ctrl+Shift+/
+        /*System.out.println("Line2");
+        System.out.println("Line3");*/
+
+        System.out.println("Line4");
+        System.out.println("Line5");
+        System.out.println("Line6");
+    }
+}
